560d5d87bb34354f16c889a39883db3a97849c9a
Ali <saida130.210@gmail.com>
2020-01-04 22:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arr, word) {
+    let empty = []
+    for(let i = 0; i < arr.length; i++) {
+        empty.push(`Thank you, ${arr[i]}, for the wonderful ${word} gift!`)
+    }
+    return empty
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num -= 1
+    }
+}
