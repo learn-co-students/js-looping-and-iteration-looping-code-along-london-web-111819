b0aef1e60a9995bba2b44a4166182e32318b8926
badogan <basri.dogan.71@gmail.com>
2020-01-05 10:21:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//   }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+ 
+// wrapGifts(gifts);
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+//   return gifts;
+}
+
+// input = [["Ada", "Brendan", "Ali"], "birthday"]
+// writeCards(input);
+function writeCards(input){
+    for (let i = 0; i < input[0].length; i++) {
+        console.log(`Thank you ${input[0][i]}, for the wonderful ${input[1]} gift!`);
+    }
+    return input;
+} 
+
+// countDown(10);
+
+function countDown(input){
+    while (input > -1 ){
+        console.log(`${input}`);
+        input--;
+    }
+}
