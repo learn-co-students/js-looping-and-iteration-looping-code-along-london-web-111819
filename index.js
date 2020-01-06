88e6491cea5ce2028d13ae00c0b664e931ceb00c
ana <anameciarobalo@gmail.com>
2020-01-06 09:50:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Code your solutions in this file
 function writeCards(array, event){
-    let thankYouMsgs = []
+    let thankYouMessages = []
     for (let i = 0; i < array.length; i++){
-       thankYouMsgs.pysh(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+       thankYouMessages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
     }
-    console.log(thankYouMsgs)
+    return(thankYouMessages)
 }
 
 function countDown(number){
-    while(number <= 0){
+    while(number >= 0){
         console.log(number);
-        number =- 1
+        number -= 1
     }
 }
